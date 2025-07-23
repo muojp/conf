7bdc4662056e85380bfdddc9d7fa7e8728237f61
Kei Nakazawa <muo@muo.jp>
2025-07-23 09:47:25 +0900

    polish

--- ubuntu-post-install/envconf.sh
+++ ubuntu-post-install/envconf.sh
@@ -1,3 +1,7 @@
+# Change user password
+echo "Please set a new password for user $(whoami):"
+passwd
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
